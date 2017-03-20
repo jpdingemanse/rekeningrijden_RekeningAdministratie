cb8ea3205e36f9e5a71d9ab30b170eee14cf2e10
Ruthger van den Eikhof <ruthger.vandeikhof@student.fontys.nl>
2017-03-20 23:12:47 +0100

    payara deploy

--- S61D_RekeningAdministratie/target/generated-sources/annotations/domain/invoice_.java
+++ S61D_RekeningAdministratie/target/generated-sources/annotations/domain/invoice_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-20T18:45:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-20T19:24:50")
 @StaticMetamodel(invoice.class)
 public class invoice_ { 
 
