3907842a0fd0a57a8c4a3721f38bc503df64af8d
Thaencharun,lino S <l.thaencharun@student.fontys.nl>
2017-03-28 12:29:00 +0200

    Change

--- S61D_RekeningAdministratie/target/generated-sources/annotations/domain/Driver_.java
+++ S61D_RekeningAdministratie/target/generated-sources/annotations/domain/Driver_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-28T10:33:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-28T12:25:32")
 @StaticMetamodel(Driver.class)
 public class Driver_ { 
 
