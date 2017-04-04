66f24acfae3edcb8637049aa033ea6ec6d64499c
Thaencharun,lino S <l.thaencharun@student.fontys.nl>
2017-04-04 10:48:02 +0200

    homepage setup

---
+++ rekeningAdministratieAngular/src/app/navbar/navbarLeft.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar-Left',
+  templateUrl: './navbarLeft.html',
+  styleUrls: ['./../app.component.css']
+})
+export class navbarLeft {
+  title = 'app works!';
+}
