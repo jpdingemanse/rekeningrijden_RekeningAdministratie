d37ec05014c0a08057623da8cecdfe9677e1540e
Thaencharun,lino S <l.thaencharun@student.fontys.nl>
2017-05-08 15:42:39 +0200

    Add ap module

--- S61D_RekeningAdministratie/target/generated-sources/annotations/domain/User_.java
+++ S61D_RekeningAdministratie/target/generated-sources/annotations/domain/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-08T13:01:44")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-08T15:07:00")
 @StaticMetamodel(User.class)
 public class User_ { 
 
--- S61D_RekeningAdministratie/target/generated-sources/annotations/domain/invoice_.java
+++ S61D_RekeningAdministratie/target/generated-sources/annotations/domain/invoice_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-08T13:01:44")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-08T15:07:00")
 @StaticMetamodel(Invoice.class)
 public class Invoice_ { 
 
