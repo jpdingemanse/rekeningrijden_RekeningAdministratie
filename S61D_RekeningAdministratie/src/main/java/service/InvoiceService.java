91329b6160871c36afa946bb106a5fb0cfbb76d3
Ruthger van den Eikhof <ruthger.vandeikhof@student.fontys.nl>
2017-06-05 17:34:26 +0200

    send invoice by queue

--- S61D_RekeningAdministratie/src/main/java/service/InvoiceService.java
+++ S61D_RekeningAdministratie/src/main/java/service/InvoiceService.java
@@ -5,6 +5,7 @@
  */
 package service;
 
+import com.google.gson.Gson;
 import dao.DriverDAO;
 import dao.InvoiceDAO;
 import dao.InvoiceRowDAO;
@@ -19,7 +20,14 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
-
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
 /**
  *
  * @author fhict
@@ -42,8 +50,17 @@ public class InvoiceService {
     VehicleDAO vehicleDAO;
     @Inject
     MovementService movementService;
+    
+    // URL of the JMS server. DEFAULT_BROKER_URL will just mean
+    // that JMS server is on localhost
+    private static String url = "tcp://192.168.24.41:61616";
+    // default broker URL is : tcp://localhost:61616"
+
+    private static String subject = "Admin"; //Queue Name
+    // You can create any/many queue names as per your requirement.
 
     public boolean createInvoice(Driver driver) {
+        Gson gson = new Gson();
         Calendar c = Calendar.getInstance();
         String month = new SimpleDateFormat("MMMM").format(c.getTime());
         int year = c.get(Calendar.YEAR);
@@ -61,10 +78,40 @@ public class InvoiceService {
             invoiceRow.setInvoice(invoice);
             invoiceRowDAO.createNewInvoiceRow(invoiceRow);
         }
+        sendMessage(gson.toJson(invoice));
         return invoiceTransmitter.SendInvoiceToRekeningrijden(invoice);
     }
 
-    public List<Invoice> getInvoiceByDriver(int id) {
+    public void sendMessage(String bericht) {
+        try{
+        // Getting JMS connection from the server and starting it
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        // JMS messages are sent and received using a Session. We will
+        // create here a non-transactional session object. If you want
+        // to use transactions you should set the first parameter to 'true'
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // Destination represents here our queue 'VALLYSOFTQ' on the
+        // JMS server. You don't have to do anything special on the
+        // server to create it, it will be created automatically.
+        Destination destination = session.createQueue(subject);
+        // MessageProducer is used for sending messages (as opposed
+        // to MessageConsumer which is used for receiving them)
+        MessageProducer producer = session.createProducer(destination);
+        // We will send a small text message saying 'Hello' in Japanese
+        TextMessage message = session.createTextMessage(bericht);
+        // Here we are sending the message!
+        producer.send(message);
+        System.out.println("Sending '" + message.getText() + "'");
+
+        connection.close();
+        } catch(JMSException ex) {
+            
+        }
+    }
+
+public List<Invoice> getInvoiceByDriver(int id) {
         return invoiceDAO.getInvoicesByDriver(id);
     }
 
